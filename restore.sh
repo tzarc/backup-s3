9e7fa96da3618b33fbb488f472ec8bebf0d2a2a8
Nick Brassel <nick@tzarc.org>
2026-05-13 20:25:44 +1000

    `--no-same-owner`

--- restore.sh
+++ restore.sh
@@ -102,7 +102,7 @@ if [[ ! -z $DATA_PATH ]]; then
 	done
 
 	echo "[INFO] Restoring data"
-	tar xf /tmp/data.tar --absolute-names --no-overwrite-dir
+	tar xf /tmp/data.tar --absolute-names --no-overwrite-dir --no-same-owner
 
 fi
 
